077bbdeddbbd1e8979b38cea0ccf91b0c2219cfd
sunyang9876 <sunyang9876@hotmail.com>
2020-01-22 21:39:44 +1000

    add mongoose

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/TWDB", { useNewUrlParser: true,  useUnifiedTopology: true })
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-const campgrounds = [
-    { name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTptz9YhkX9INTI6F4RaiSnhLCJDbqcl4qqFuJZyHi6--PnrPCj&s" },
-    { name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOnXO5O-5s0EcQCBxL5JMKmg9GgU9M2D7c5tzriDISy_oDkvVB&s" },
-    { name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYS1T2yCqPdvz2aW_1HZ7HFeuq0zAyTltOqsxz40gO4Ca8K72cPQ&s" },
-    { name: "Giovanie Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZtSFPkXopIAg-PP60UvrvXOahdY-btwV-GMaVzjHP0UazAFoz&s" },
-    { name: "Rashford", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTptz9YhkX9INTI6F4RaiSnhLCJDbqcl4qqFuJZyHi6--PnrPCj&s" },
-    { name: "Matrial", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOnXO5O-5s0EcQCBxL5JMKmg9GgU9M2D7c5tzriDISy_oDkvVB&s" },
-    { name: "Mata", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYS1T2yCqPdvz2aW_1HZ7HFeuq0zAyTltOqsxz40gO4Ca8K72cPQ&s" },
-    { name: "James", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZtSFPkXopIAg-PP60UvrvXOahdY-btwV-GMaVzjHP0UazAFoz&s" },
-];
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image:String
+})
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOnXO5O-5s0EcQCBxL5JMKmg9GgU9M2D7c5tzriDISy_oDkvVB&s"
+// }, (err, campground) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log('NEWLY CREATED CAMPGROUND');
+//         console.log(campground);
+// })
+
+// const campgrounds = [
+//     { name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTptz9YhkX9INTI6F4RaiSnhLCJDbqcl4qqFuJZyHi6--PnrPCj&s" },
+//     { name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOnXO5O-5s0EcQCBxL5JMKmg9GgU9M2D7c5tzriDISy_oDkvVB&s" },
+//     { name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYS1T2yCqPdvz2aW_1HZ7HFeuq0zAyTltOqsxz40gO4Ca8K72cPQ&s" },
+//     { name: "Giovanie Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZtSFPkXopIAg-PP60UvrvXOahdY-btwV-GMaVzjHP0UazAFoz&s" },
+//     { name: "Rashford", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTptz9YhkX9INTI6F4RaiSnhLCJDbqcl4qqFuJZyHi6--PnrPCj&s" },
+//     { name: "Matrial", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOnXO5O-5s0EcQCBxL5JMKmg9GgU9M2D7c5tzriDISy_oDkvVB&s" },
+//     { name: "Mata", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYS1T2yCqPdvz2aW_1HZ7HFeuq0zAyTltOqsxz40gO4Ca8K72cPQ&s" },
+//     { name: "James", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZtSFPkXopIAg-PP60UvrvXOahdY-btwV-GMaVzjHP0UazAFoz&s" },
+// ];
 
 
 app.get("/", function(req, res){
@@ -22,8 +42,13 @@ app.get("/", function(req, res){
 })
 
 app.get("/campgrounds", function (req, res) {
+    Campground.find({}, (err, campgrounds) => {
+        if (err) {
+            console.log(err);
+        }
+        res.render("campgrounds",{campgrounds});
+    })
 
-    res.render("campgrounds",{campgrounds});
 })
 
 app.get("/campgrounds/new", function (req, res) {
@@ -34,8 +59,13 @@ app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     const newCampground = { name, image };
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    // campgrounds.push(newCampground);
+    Campground.create(newCampground, (err, newlyCreated) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect("/campgrounds");
+    })
 })
 
 app.listen(3000, function() {
