0227beea81b41779cd78b11358c2bd5c20bafb05
sunyang9876 <sunyang9876@hotmail.com>
2020-02-05 10:14:15 +1000

    comment edit and update

--- routes/comments.js
+++ routes/comments.js
@@ -36,6 +36,26 @@ router.post("/", isLoggedIn, (req, res) => {
     })
 })
 
+router.get("/:comment_id/edit", (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err) {
+            res.redirect("back")
+        } else {
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+        }
+    })
+})
+
+router.put("/:comment_id", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if (err) {
+            res.redirect("back")
+        } else {
+            res.redirect("/campgrounds/" + req.params.id)
+        }
+    })
+})
+
 function isLoggedIn(req, res, next){
     if (req.isAuthenticated()) {
         return next();
